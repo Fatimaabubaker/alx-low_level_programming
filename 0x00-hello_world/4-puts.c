67d6866d2022471880d45e621a5ed6f8d970d206
Fatimaabubaker <Mohmmedabdo4@gmail.com>
2024-02-05 06:36:38 -0800

    finished

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	put("\"progrmming is like building a multilingual puzzle");       	  return (0);
+}	
