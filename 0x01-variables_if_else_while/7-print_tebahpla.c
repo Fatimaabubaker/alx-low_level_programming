1a210071fe34b91cc0e62d2fcdf1a15df2969fc4
Fatmaabubaker <Mohmmedabdo4@gmail.com>
2024-05-22 05:33:37 -0700

    print alphabets from z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabets reversely
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+
+	return (0);
+}
