e4e6f63851f3491cf65546958ea685fc47c2f8ca
Fatmaabubaker <Mohmmedabdo4@gmail.com>
2024-05-29 03:36:59 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,7 +1,8 @@
-#include <main.h>
-
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - write the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
