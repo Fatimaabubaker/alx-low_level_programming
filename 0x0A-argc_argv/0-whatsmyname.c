6c52f8c69e27a9d395f71ce0666355199826ab69
Fatmaabubaker <Mohmmedabdo4@gmail.com>
2024-06-02 01:31:51 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array that contains the command line arg..
  * Return: (0) when successful
  */
-int main(int agrc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
