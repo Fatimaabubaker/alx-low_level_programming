1c345bbe82ed94660e7d20b0f75406eb1d07e3c3
Fatmaabubaker <Mohmmedabdo4@gmail.com>
2024-06-03 12:17:15 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget mandatory

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _puts_recursion - prints a string followed by a new line 
- * @s: string
+ * _puts_recursion - recursive function to print to screen
  *
- * Return: On success 1 .
- * On error, -1 is returned, and errno is set appropriately.
+ * @s: pointer to the string
+ *
+ * Return: nothing
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != '\0')
 	{
-		_putchar ('\n);
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s+1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
 }
